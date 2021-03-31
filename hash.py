cc6c62e1d382eec56f656976f8520d27de262de4
babadum <superatum1@gmail.com>
2021-03-31 17:20:31 -0400

    made some changes to hash.py

--- hash.py
+++ hash.py
@@ -4,6 +4,11 @@ import hashinfunc
 for dirpath, dirs, files in os.walk("/"):
     for filename in files:
         fname = os.path.join(dirpath,filename)
+        hashedFile = None
         with open(fname) as myfile:
-            hashFile(myfile.read())
+            try:
+                hashedFile = hashinfunc.hashFile(myfile.read())
+            except:
+                print("error hashing")
+            print(hashedFile)
             
