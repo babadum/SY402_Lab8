271266bc954863626d0cea48744a7ecde14154ec
babadum <superatum1@gmail.com>
2021-04-06 08:48:08 -0400

    added unhashables list

--- hash.py
+++ hash.py
@@ -1,14 +1,20 @@
 import os
 import hashinfunc
 
+
+unhashables = ["/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
 for dirpath, dirs, files in os.walk("/"):
     for filename in files:
         fname = os.path.join(dirpath,filename)
         hashedFile = None
+        
         with open(fname) as myfile:
             try:
                 hashedFile = hashinfunc.hashFile(myfile.read())
-            except:
-                print("error hashing")
-            print(hashedFile)
+                print(hashedFile)
+            except Exception as e:
+                print(e)
+            finally:
+            	pass
+            
             
