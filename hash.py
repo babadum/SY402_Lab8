2ab9815b7eedab98d8387bc9a37fceb28aa39620
babadum <superatum1@gmail.com>
2021-04-06 09:26:38 -0400

    fixed some bugs. May not be finished unsure what else needs to be done

--- hash.py
+++ hash.py
@@ -2,19 +2,29 @@ import os
 import hashinfunc
 
 
-unhashables = ["/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
+unhashables = ["/dev", "/proc", "/run", "/sys", "/tmp", "/var/run", "/var/lib"]
 for dirpath, dirs, files in os.walk("/"):
     for filename in files:
         fname = os.path.join(dirpath,filename)
         hashedFile = None
-        
-        with open(fname) as myfile:
+        #print(dirpath)
+        if not dirpath in unhashables:
             try:
-                hashedFile = hashinfunc.hashFile(myfile.read())
-                print(hashedFile)
+                #with open(fname) as myfile:
+                try:
+                    print(dirpath)
+
+                    hashedFile = hashinfunc.hashFile(fname)
+                    print(hashedFile)
+                except Exception as e:
+                    print(e)
+                finally:
+                   pass
             except Exception as e:
                 print(e)
             finally:
-            	pass
+                pass
+        else:
+            break
             
             
